528f3e7d88bfcbfee3827135197ebd792b3825db
David Fokkema <davidfokkema@icloud.com>
2023-11-11 19:05:28 +0100

    Load conda environments

--- src/fix_conda_ssl/app.py
+++ src/fix_conda_ssl/app.py
@@ -1,8 +1,8 @@
 import asyncio
+import subprocess
 
 from textual import work
 from textual.app import App, ComposeResult
-from textual.widget import Widget
 from textual.widgets import Footer, Header, ListItem, ListView, Static
 
 INFO_MSG = """It is that time of year again: conda environments are broken because of SSL library problems. In my experience, this is the case on Windows in either the base channel or conda-forge, every Fall for the past few years. I know, because I'm teaching a Fall coding course and my students can't install packages or use pipx-installed applications. Almost. Every. Year. Sigh."""
@@ -40,9 +40,19 @@ class FixCondaSSLApp(App[None]):
     @work
     async def load_conda_environments(self) -> None:
         list_view = self.query_one(ListView)
-        for x in range(5):
-            await list_view.append(CondaEnvironment(f"Environment {x}", path=None))
+
+        process = await asyncio.create_subprocess_shell(
+            "conda env list", stdout=subprocess.PIPE
+        )
+        output = await process.stdout.read()
         list_view.loading = False
+        for line in output.decode().splitlines():
+            if line.startswith("#"):
+                continue
+            fields = line.split()
+            if len(fields) >= 2:
+                env, *_, path = fields
+                await list_view.append(CondaEnvironment(f"{env}", path))
 
 
 app = FixCondaSSLApp
